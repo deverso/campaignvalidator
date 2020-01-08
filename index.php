6c528b151703abf600f8b4dde412aef092d80f62
deverso <deverso@gmail.com>
2020-01-08 13:29:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,11 @@ if (isset($_POST['term'])) {
         background-color: #d6d9dc;
       }
     </style>
+    
+    <!-- Bootstrap Javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <title>Campaign Validator</title>
   </head>
   <body>
