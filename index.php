a3adcedd273ee91a6900e83cfbeb209d6b97ad4f
deverso <deverso@gmail.com>
2020-01-08 12:01:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+$campaignPatterns = '(^[\d]{8,9}$|^[\w]{24}$|.*\.BRM.*|KD[\d]{6}$)';
+$result["display"] = 'none';
+
+if (isset($_POST['term'])) {
+  $result["display"] = 'block';
+  $result["type"] = 'danger';
+  $result["text"] = 'Este é um valor <b>inválido</b> para campanhas.';
+  if (preg_match($campaignPatterns, $_POST['term']) == 1 ) {
+    $result["type"] = 'success';
+    $result["text"] = 'Este é um valor <b>valido</b> para campanhas.';
+  }
+}
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -13,34 +28,20 @@
   <body>
     <div class="border border-primary rounded">
       <h1>Campaign Validator</h1>
-      <div class="alert alert-success" role="alert">
-        A simple success alert—check it out!
+      <div class="alert alert-<?=$result['type']?>" role="alert" style="display: <?=$result['display']?>">
+        <?=$result['text']?>
       </div>
       <form>
         <div class="form-group">
           <label for="exampleInputEmail1">Campaign</label>
           <input type="text" name="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <small id="emailHelp" class="form-text text-muted">ser numérico de 8 à 9 caracteres</small>
-          <small id="emailHelp" class="form-text text-muted">ser alfanumérico de 24 caracteres (não pode ter espaço)</small>
-          <small id="emailHelp" class="form-text text-muted">conter em qualquer posição o texto .BRM ex: Campain.home-70%off.BRN-thing</small>
-          <small id="emailHelp" class="form-text text-muted">começar com o prefixo KD e ser seguido de 6 dpigitos numéridos ex:KD123456</small>
+          <small id="emailHelp" class="form-text text-muted">&bull; ser numérico de 8 à 9 caracteres</small>
+          <small id="emailHelp" class="form-text text-muted">&bull; ser alfanumérico de 24 caracteres (não pode ter espaço)</small>
+          <small id="emailHelp" class="form-text text-muted">&bull; conter em qualquer posição o texto .BRM ex: Campain.home-70%off.BRN-thing</small>
+          <small id="emailHelp" class="form-text text-muted">&bull; começar com o prefixo KD e ser seguido de 6 dpigitos numéridos ex:KD123456</small>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Validar</button>
       </form>
     </div>
   </body>
 </html>
-
-<?php
-$campaignPatterns = '(^[\d]{8,9}$|^[\w]{24}$|.*\.BRM.*|KD[\d]{6}$)';
-$result["display"] = 'none';
-
-if (isset($_POST['term'])) {
-  $result["display"] = 'block';
-  $result["type"] = 'danger';
-  $result["text"] = 'Este é um valor <b>inválido</b> para campanhas.';
-  if (preg_match(campaignPatterns, $_POST['term']) == 1 ) {
-    $result["type"] = 'success';
-    $result["text"] = 'Este é um valor <b>valido</b> para campanhas.';
-  }
-}
