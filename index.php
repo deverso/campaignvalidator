d336f67800505749f1b544865a5787cbafd22fb1
deverso <deverso@gmail.com>
2020-01-08 11:56:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,46 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <title>Campaign Validator</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<div class="container">
-  <h2>Campaign Format Validator</h2>
-  <form action="/action_page.php">
-    <div class="form-group">
-      <label for="email">Campaign:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-        <small id="emailHelp" class="form-text text-muted">&bullet; ser numérico de 8 à 9 caracteres</small>
-        <small id="emailHelp" class="form-text text-muted">&bullet; ser alfanumérico de 24 caracteres (não pode ter espaço)</small>
-        <small id="emailHelp" class="form-text text-muted">&bullet; conter em qualquer posição o texto .BRM ex: Campain.home-70%off.BRN-thing</small>
-        <small id="emailHelp" class="form-text text-muted">&bullet; começar com o prefixo KD e ser seguido de 6 dpigitos numéridos ex:KD123456</small>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="border border-primary rounded">
+      <h1>Campaign Validator</h1>
+      <div class="alert alert-success" role="alert">
+        A simple success alert—check it out!
+      </div>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Campaign</label>
+          <input type="text" name="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <small id="emailHelp" class="form-text text-muted">ser numérico de 8 à 9 caracteres</small>
+          <small id="emailHelp" class="form-text text-muted">ser alfanumérico de 24 caracteres (não pode ter espaço)</small>
+          <small id="emailHelp" class="form-text text-muted">conter em qualquer posição o texto .BRM ex: Campain.home-70%off.BRN-thing</small>
+          <small id="emailHelp" class="form-text text-muted">começar com o prefixo KD e ser seguido de 6 dpigitos numéridos ex:KD123456</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-</div>
-
-</body>
+  </body>
 </html>
+
+<?php
+$campaignPatterns = '(^[\d]{8,9}$|^[\w]{24}$|.*\.BRM.*|KD[\d]{6}$)';
+$result["display"] = 'none';
+
+if (isset($_POST['term'])) {
+  $result["display"] = 'block';
+  $result["type"] = 'danger';
+  $result["text"] = 'Este é um valor <b>inválido</b> para campanhas.';
+  if (preg_match(campaignPatterns, $_POST['term']) == 1 ) {
+    $result["type"] = 'success';
+    $result["text"] = 'Este é um valor <b>valido</b> para campanhas.';
+  }
+}
